8512e9c3ebf0dd747dd038d75cafe0a2213b13d8
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2025-10-01 15:15:10 +0200

    Changed the background color for the main-content buttons

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,30 +1,36 @@
 import React from "react";
 import { LiaShoppingCartSolid } from "react-icons/lia";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
-    <div
-      style={{
-        height: "75vh",
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      {/* <h1>Welcome to the Home Page</h1> */}
-
-      <div className="homeNavBar">
-        <LiaShoppingCartSolid size={70} />
-        <ul>
-          <li>Home</li>
-          <li>Categories</li>
-          <li>Cart</li>
-          <li>Favourite</li>
-          <li>Settings</li>
-        </ul>{" "}
-      </div>
-      <div></div>
+    <div className="homeLayout">
+      <nav className="homeNavBar">
+        <div className="homeBrand">
+          <LiaShoppingCartSolid size={60} />
+        </div>
+        <ul className="homeNavList">
+          <li>
+            <Link className="homeNavButton" to="/home">Home</Link>
+          </li>
+          <li>
+            <Link className="homeNavButton" to="/categories">Categories</Link>
+          </li>
+          <li>
+            <Link className="homeNavButton" to="/cart">Cart</Link>
+          </li>
+          <li>
+            <Link className="homeNavButton" to="/favourites">Favourite</Link>
+          </li>
+          <li>
+            <Link className="homeNavButton" to="/settings">Settings</Link>
+          </li>
+        </ul>
+      </nav>
+      <section className="homeContent">
+        <h2>Welcome back</h2>
+        <p>Add items to your shopping list, browse categories, and manage your cart.</p>
+      </section>
     </div>
   );
 }
