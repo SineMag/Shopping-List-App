3310e414603b63b2e444c9e7e3978f648a403e41
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2025-10-02 08:29:44 +0200

    feat: added login & register buttons

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,28 +8,51 @@ export default function HomePage() {
       <nav className="homeNavBar">
         <div className="homeBrand">
           <LiaShoppingCartSolid size={60} />
+          <ul>
+            <li>
+              <Link className="homeNavButton" to="/register">
+                Register
+              </Link>
+              <br />
+            </li>
+            <li>
+              <Link className="homeNavButton" to="/login">
+                Login
+              </Link>
+            </li>
+          </ul>
         </div>
         <ul className="homeNavList">
           <li>
-            <Link className="homeNavButton" to="/home">Home</Link>
+            <Link className="homeNavButton" to="/home">
+              Home
+            </Link>
           </li>
           <li>
-            <Link className="homeNavButton" to="/categories">Categories</Link>
+            <Link className="homeNavButton" to="/categories">
+              Categories
+            </Link>
           </li>
           <li>
-            <Link className="homeNavButton" to="/cart">Cart</Link>
+            <Link className="homeNavButton" to="/cart">
+              Cart
+            </Link>
           </li>
           <li>
-            <Link className="homeNavButton" to="/favourites">Favourite</Link>
+            <Link className="homeNavButton" to="/favourites">
+              Favourite
+            </Link>
           </li>
           <li>
-            <Link className="homeNavButton" to="/settings">Settings</Link>
+            <Link className="homeNavButton" to="/settings">
+              Settings
+            </Link>
           </li>
         </ul>
       </nav>
       <section className="homeContent">
-        <h2>Welcome back</h2>
-        <p>Add items to your shopping list, browse categories, and manage your cart.</p>
+        <h1>Welcome </h1>
+        <h2>Get ready to start shopping!</h2>
       </section>
     </div>
   );
